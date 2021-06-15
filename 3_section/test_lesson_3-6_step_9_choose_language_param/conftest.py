bb9a2a879f4f9642ee214953e2b027539d6ffdd1
Vladislav <radostmoyaaa@mail.ru>
2021-06-15 12:10:37 +0300

    Little changes

--- 3_section/test_lesson_3-6_step_9_choose_language_param/conftest.py
+++ 3_section/test_lesson_3-6_step_9_choose_language_param/conftest.py
@@ -8,17 +8,13 @@ def pytest_addoption(parser):
     parser.addoption('--language', default='ru')
 
 
-@pytest.fixture(scope='function')
-def config(request):
-    language = request.config.getoption("--language")
-    url = 'http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/'.format(language)
-    return {'url': url}
-
-
 @pytest.fixture(scope="function")
 def browser(request):  # Фикстура драйвера
+    user_language = request.config.getoption("--language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     manager = ChromeDriverManager(version='latest')  # Инициализируем менеджер загрузки драйвера chrome
-    browser = webdriver.Chrome(executable_path=manager.install())
+    browser = webdriver.Chrome(executable_path=manager.install(), options=options)
     yield browser
     browser.close()
 
